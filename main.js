e28984c01ad8e54b8029360f6dc9f6366f36f87c
azadorozhniy <artem.zadorozhniy@gmail.com>
2014-03-25 00:33:12 +0200

    Added required libs for texmplate (boostrap, jquery)

--- main.js
+++ main.js
@@ -2,10 +2,31 @@ require.config({
   paths: {
     angular: '../bower_components/angular/angular',
     uiRouter: '../bower_components/angular-ui-router/release/angular-ui-router',
+    angularScenario: '../bower_components/angular-scenario/angular-scenario',
     angularMocks: '../bower_components/angular-mocks/angular-mocks',
+    jquery: '../bower_components/jquery/dist/jquery',
+    d3: '../bower_components/d3/d3',
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
+    angularBootstrap: '../bower_components/angular-bootstrap/ui-bootstrap',
+    angularBootstrapTpl: '../bower_components/angular-bootstrap/ui-bootstrap-tpls',
     text: '../bower_components/requirejs-text/text'
   },
   shim: {
+    'jquery': {
+      'exports': 'jquery'
+    },
+    'bootstrap': {
+      deps: ['jquery']
+    },
+    'angularBootstrap': {
+      deps: ['jquery', 'bootstrap']
+    },
+    'angularBootstrapTpl': {
+      deps: ['angularBootstrap']
+    },
+    'd3': {
+      'exports': 'd3'
+    },
     'angular': {
       'exports': 'angular'
     },
@@ -18,6 +39,7 @@ require.config({
     }
   },
   priority: [
+    "jquery",
     "angular"
   ]
 });
@@ -25,11 +47,14 @@ require.config({
 //http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
 //window.name = "NG_DEFER_BOOTSTRAP!";
 
-require([
+require(['jquery',
   'angular',
   './app/app',
-  'text!app/templates/layout.html'
-], function (angular, app, layout) {
+  'text!app/templates/layout.html',
+  'bootstrap',
+  'angularBootstrap',
+  'angularBootstrapTpl'
+], function ($, angular, app, layout) {
   'use strict';
 
   angular.element().ready(function () {
