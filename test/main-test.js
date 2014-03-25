2110a5399ec9801feabc3d992ac6f946de901774
azadorozhniy <artem.zadorozhniy@gmail.com>
2014-03-25 18:11:30 +0200

    Fixed test for require js

--- test/main-test.js
+++ test/main-test.js
@@ -20,10 +20,33 @@ require.config({
   paths: {
     angular: '../base/bower_components/angular/angular',
     uiRouter: '../base/bower_components/angular-ui-router/release/angular-ui-router',
+    angularScenario: '../base/bower_components/angular-scenario/angular-scenario',
     angularMocks: '../base/bower_components/angular-mocks/angular-mocks',
-    text: '../base/bower_components/requirejs-text/text'
+    jquery: '../base/bower_components/jquery/dist/jquery',
+    d3: '../base/bower_components/d3/d3',
+    bootstrap: '../base/bower_components/bootstrap/dist/js/bootstrap',
+    angularBootstrap: '../base/bower_components/angular-bootstrap/ui-bootstrap',
+    angularBootstrapTpl: '../base/bower_components/angular-bootstrap/ui-bootstrap-tpls',
+    text: '../base/bower_components/requirejs-text/text',
+    morris: '../base/bower_components/morris.js/morris',
+    raphael: '../base/bower_components/raphael/raphael'
   },
   shim: {
+    'jquery': {
+      'exports': 'jquery'
+    },
+    'bootstrap': {
+      deps: ['jquery']
+    },
+    'angularBootstrap': {
+      deps: ['jquery', 'bootstrap']
+    },
+    'angularBootstrapTpl': {
+      deps: ['angularBootstrap']
+    },
+    'd3': {
+      'exports': 'd3'
+    },
     'angular': {
       'exports': 'angular'
     },
@@ -33,8 +56,16 @@ require.config({
     'angularMocks': {
       deps: ['angular'],
       'exports': 'angular.mock'
+    },
+    'morris' : {
+      deps: ['raphael'],
+      exports: 'Morris'
     }
-  }
+  },
+  priority: [
+    "jquery",
+    "angular"
+  ]
 });
 
 require(allTestFiles, function () {
